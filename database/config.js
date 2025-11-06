ed7167367346fcdaba8e832495831355bd365ba5
lodubot <jaihacku@gmail.com>
2025-11-06 21:13:46 +0530

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8551825717:AAGpUQblCHAG8t3G4_OdIIei6bJatIedHWs",  // Ubah Jadi Token Bot Mu !!!
+  owner: "8390074452", // Ubah Jadi Id Mu !!!
+  port: "1466", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "167.172.64.240" // Ubah Jadi Ip Vps Mu !!!
+};
